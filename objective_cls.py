9c13ad3f3fc3b559795c316b20f76aa96046eb1c
Ben Harding <bharding@ualberta.ca>
2022-10-03 14:06:23 -0700

    update indicator transforms

--- objective_cls.py
+++ objective_cls.py
@@ -21,6 +21,9 @@ class Objective(object):
     def __init__(self, Y):
         # pool of Gaussian factors to be mixed
         self.Y = Y
+        self.nreals = 1
+        if Y.ndim > 2:
+            self.nreals = Y.shape[-1]
 
     def __call__(self, x, args):  # starargs for scipy
         # returns scalar objective
@@ -34,9 +37,9 @@ class Objective(object):
         xvario = update_variogram(AL, xlag)
         yvario = update_variogram(AL, ylag)
         zvario = update_variogram(AL, zlag)
-        xlag_wts = inv_dist(lx, 1.0)
-        ylag_wts = inv_dist(ly, 1.0)
-        zlag_wts = inv_dist(lz, 1.0)
+        xlag_wts = inv_dist(lx, 2.5)
+        ylag_wts = inv_dist(ly, 2.5)
+        zlag_wts = inv_dist(lz, 2.5)
         sill = np.var(AL)
         objv = 0.0
         objv += expvario_loss(xvario, tx, lx, xlag_wts, sill)
@@ -44,14 +47,16 @@ class Objective(object):
         objv += expvario_loss(zvario, tz, lz, zlag_wts, sill)
         return objv * scale
 
-    def objective_ivario(self, AL, targets, lag_dicts, lags, thresholds, scale):
+    def objective_ivario(
+        self, AL_i, ivars, targets, lag_dicts, lags, thresholds, scale
+    ):
         """continuous variogram objective function"""
         xlag, ylag, zlag = lag_dicts
         lx, ly, lz = lags
-        xlag_wts = inv_dist(lx, 1.0)
-        ylag_wts = inv_dist(ly, 1.0)
-        zlag_wts = inv_dist(lz, 1.0)
-        AL_i, ivars = indicator_transform(AL, thresholds.values())
+        xlag_wts = inv_dist(lx, 2.5)
+        ylag_wts = inv_dist(ly, 2.5)
+        zlag_wts = inv_dist(lz, 2.5)
+        # AL_i, ivars = indicator_transform(AL, thresholds.values())
         nq = len(thresholds)
         objv = 0.0
         for j, q in enumerate(thresholds):
@@ -71,15 +76,15 @@ class Objective(object):
         return objv * scale
 
     def objective_runs(
-        self, AL, target, thresholds, runs_above, maxrun, dhids, dh_lens, scale
+        self, AL_i, target, thresholds, runs_above, maxrun, dhids, dh_lens, scale
     ):
         """cumulative run frequency objective fucntion"""
-        AL_i, ivars = indicator_transform(AL, thresholds.values())
+        # AL_i, ivars = indicator_transform(AL, thresholds.values())
         nq = len(thresholds)
         objv = 0.0
         for j, q in enumerate(thresholds):
             # X = AL_i[:, j].reshape(ncomps, ndh, order="F")
-            X = self._reshape_vector(AL_i[:, j], dhids, dh_lens, -1)
+            X = self._reshape_vector(AL_i[:, j], dhids, dh_lens, np.nan)
             temp_runs = np.zeros(maxrun)
             ndh = len(set(dhids))
             for n in range(ndh):
@@ -90,21 +95,88 @@ class Objective(object):
             objv += self.objective_histogram(temp_runs, target[q], scale)  # * (1 / nq)
         return objv
 
-    def objective_npoint(self, AL, target, thresholds, nstep, dhids, dh_lens, scale):
+    def objective_npoint(self, AL_i, target, thresholds, nstep, dhids, dh_lens, scale):
         """n-point connectivity objective fucntion"""
-        AL_i, ivars = indicator_transform(AL, thresholds.values())
+        # AL_i, ivars = indicator_transform(AL, thresholds.values())
         objv = 0.0
         for j, q in enumerate(thresholds):
-            X = self._reshape_vector(AL_i[:, j], dhids, dh_lens, -1)
+            X = self._reshape_vector(AL_i[:, j], dhids, dh_lens, np.nan)
             npoint = n_pt_conn(X, nstep)
             objv += self.objective_histogram(npoint, target[q], scale)
         return objv
+
+    # def network_lmr_objective(
+    #     self,
+    #     x,
+    #     parameters,
+    #     connections,
+    #     biases,
+    #     target_vario,
+    #     target_ivario,
+    #     target_runs,
+    #     target_npoint,
+    #     lag_dicts,
+    #     lags,
+    #     maxrun,
+    #     nstep,
+    #     dhids,
+    #     dh_lens,
+    #     runs_above,
+    #     thresholds,
+    #     afunc,
+    #     objscale,
+    #     vario,
+    #     ivario,
+    #     runs,
+    #     npoint,
+    # ):
+    #     """objective function for network lmr"""
+
+    #     # caluclate the forward pass
+    #     params = vector_to_matrices(parameters, connections, biases, x)
+    #     AL = linear_forward(self.Y, params, afunc)
+    #     AL_i, ivars = indicator_transform(AL, thresholds.values())
+
+    #     # initialize objective value
+    #     objv = 0.0
+
+    #     # continuous variogram
+    #     if vario:
+    #         objv += self.objective_vario(AL, target_vario, lag_dicts, lags, objscale[0])
+
+    #     # indicator variogram
+    #     if ivario:
+    #         objv += self.objective_ivario(
+    #             AL_i, ivars, target_ivario, lag_dicts, lags, thresholds, objscale[1]
+    #         )
+
+    #     # cumulative runs
+    #     if runs:
+    #         objv += self.objective_runs(
+    #             AL_i,
+    #             target_runs,
+    #             thresholds,
+    #             runs_above,
+    #             maxrun,
+    #             dhids,
+    #             dh_lens,
+    #             objscale[2],
+    #         )
+
+    #     # npoint connectivity runs
+    #     if npoint:
+    #         objv += self.objective_npoint(
+    #             AL_i, target_npoint, thresholds, nstep, dhids, dh_lens, objscale[3]
+    #         )
+
+    #     return objv
 
     def network_lmr_objective(
         self,
         x,
         parameters,
         connections,
+        biases,
         target_vario,
         target_ivario,
         target_runs,
@@ -127,48 +199,65 @@ class Objective(object):
         """objective function for network lmr"""
 
         # caluclate the forward pass
-        params = vector_to_matrices(parameters, connections, x)
-        AL = linear_forward(self.Y, params, afunc)
+        params = vector_to_matrices(parameters, connections, biases, x)
 
         # initialize objective value
         objv = 0.0
 
-        # continuous variogram
-        if vario:
-            objv += self.objective_vario(AL, target_vario, lag_dicts, lags, objscale[0])
+        for ireal in range(self.nreals):
 
-        # indicator variogram
-        if ivario:
-            objv += self.objective_ivario(
-                AL, target_ivario, lag_dicts, lags, thresholds, objscale[1]
-            )
+            AL = linear_forward(self.Y[..., ireal], params, afunc)
+            AL_i, ivars = indicator_transform(AL, thresholds.values())
 
-        # cumulative runs
-        if runs:
-            objv += self.objective_runs(
-                AL,
-                target_runs,
-                thresholds,
-                runs_above,
-                maxrun,
-                dhids,
-                dh_lens,
-                objscale[2],
-            )
+            # continuous variogram
+            if vario:
+                objv += self.objective_vario(
+                    AL, target_vario, lag_dicts, lags, objscale[0]
+                )
 
-        # npoint connectivity runs
-        if npoint:
-            objv += self.objective_npoint(
-                AL, target_npoint, thresholds, nstep, dhids, dh_lens, objscale[3]
-            )
+            # indicator variogram
+            if ivario:
+                objv += self.objective_ivario(
+                    AL_i, ivars, target_ivario, lag_dicts, lags, thresholds, objscale[1]
+                )
 
-        return objv
+            # cumulative runs
+            if runs:
+                objv += self.objective_runs(
+                    AL_i,
+                    target_runs,
+                    thresholds,
+                    runs_above,
+                    maxrun,
+                    dhids,
+                    dh_lens,
+                    objscale[2],
+                )
+
+            # npoint connectivity runs
+            if npoint:
+                objv += self.objective_npoint(
+                    AL_i, target_npoint, thresholds, nstep, dhids, dh_lens, objscale[3]
+                )
+
+        return objv / self.nreals
 
     def init_vario(
-        self, targets, lag_dicts, lags, connections, parameters, bounds, afunc, rng,
+        self,
+        targets,
+        lag_dicts,
+        lags,
+        connections,
+        biases,
+        parameters,
+        bounds,
+        afunc,
+        rng,
     ):
         """initialize continuous variogram objective function"""
-        AL = self._random_forward_pass(parameters, afunc, bounds, connections, rng)
+        AL = self._random_forward_pass(
+            parameters, afunc, bounds, connections, biases, rng
+        )
         return self.objective_vario(AL, targets, lag_dicts, lags, scale=1.0)
 
     def init_ivario(
@@ -178,15 +267,19 @@ class Objective(object):
         lags,
         thresholds,
         connections,
+        biases,
         parameters,
         bounds,
         afunc,
         rng,
     ):
         """initialize continuous variogram objective function"""
-        AL = self._random_forward_pass(parameters, afunc, bounds, connections, rng)
+        AL = self._random_forward_pass(
+            parameters, afunc, bounds, connections, biases, rng
+        )
+        AL_i, ivars = indicator_transform(AL, thresholds.values())
         return self.objective_ivario(
-            AL, targets, lag_dicts, lags, thresholds, scale=1.0
+            AL_i, ivars, targets, lag_dicts, lags, thresholds, scale=1.0
         )
 
     def init_runs(
@@ -198,15 +291,19 @@ class Objective(object):
         thresholds,
         runs_above,
         connections,
+        biases,
         parameters,
         bounds,
         afunc,
         rng,
     ):
         """initialize runs objective"""
-        AL = self._random_forward_pass(parameters, afunc, bounds, connections, rng)
+        AL = self._random_forward_pass(
+            parameters, afunc, bounds, connections, biases, rng
+        )
+        AL_i, ivars = indicator_transform(AL, thresholds.values())
         return self.objective_runs(
-            AL, target, thresholds, runs_above, maxrun, dhids, dh_lens, scale=1.0
+            AL_i, target, thresholds, runs_above, maxrun, dhids, dh_lens, scale=1.0
         )
 
     def init_npoint(
@@ -217,20 +314,25 @@ class Objective(object):
         dhids,
         dh_lens,
         connections,
+        biases,
         parameters,
         bounds,
         afunc,
         rng,
     ):
         """initialize n-point connectivity objective"""
-        AL = self._random_forward_pass(parameters, afunc, bounds, connections, rng)
+        AL = self._random_forward_pass(
+            parameters, afunc, bounds, connections, biases, rng
+        )
+        AL_i, ivars = indicator_transform(AL, thresholds.values())
         return self.objective_npoint(
-            AL, target, thresholds, nstep, dhids, dh_lens, scale=1.0
+            AL_i, target, thresholds, nstep, dhids, dh_lens, scale=1.0
         )
 
     def objective_scaling(
         self,
         connections,
+        biases,
         parameters,
         bounds,
         lag_dicts,
@@ -268,6 +370,7 @@ class Objective(object):
                 lag_dicts,
                 lags,
                 connections,
+                biases,
                 parameters,
                 bounds,
                 afunc,
@@ -280,6 +383,7 @@ class Objective(object):
                 lags,
                 thresholds,
                 connections,
+                biases,
                 parameters,
                 bounds,
                 afunc,
@@ -294,6 +398,7 @@ class Objective(object):
                 thresholds,
                 runs_above,
                 connections,
+                biases,
                 parameters,
                 bounds,
                 afunc,
@@ -307,6 +412,7 @@ class Objective(object):
                 dhids,
                 dh_lens,
                 connections,
+                biases,
                 parameters,
                 bounds,
                 afunc,
@@ -316,7 +422,10 @@ class Objective(object):
         for m in range(maxpert):
 
             # caluclate the forward pass
-            AL = self._random_forward_pass(parameters, afunc, bounds, connections, rng)
+            AL = self._random_forward_pass(
+                parameters, afunc, bounds, connections, biases, rng
+            )
+            AL_i, ivars = indicator_transform(AL, thresholds.values())
 
             # approximate objective contributions
             if vario:
@@ -329,7 +438,13 @@ class Objective(object):
 
             if ivario:
                 temp_obj_ivario = self.objective_ivario(
-                    AL, ivario_target, lag_dicts, lags, thresholds, scale=objscale[1],
+                    AL_i,
+                    ivars,
+                    ivario_target,
+                    lag_dicts,
+                    lags,
+                    thresholds,
+                    scale=objscale[1],
                 )
                 if temp_obj_ivario < 0.0:
                     temp_obj_ivario = objinit[1]
@@ -337,7 +452,7 @@ class Objective(object):
 
             if runs:
                 temp_obj_runs = self.objective_runs(
-                    AL,
+                    AL_i,
                     runs_target,
                     thresholds,
                     runs_above,
@@ -352,7 +467,7 @@ class Objective(object):
 
             if npoint:
                 temp_obj_npt = self.objective_npoint(
-                    AL,
+                    AL_i,
                     npoint_target,
                     thresholds,
                     nstep,
@@ -383,11 +498,31 @@ class Objective(object):
 
         return objscale
 
-    def _random_forward_pass(self, parameters, afunc, bounds, connections, rng):
+    def generate_bounds(self, init_params, connections):
+        """generate weight bounds based on intialized values"""
+        bounds = []
+        L = len(init_params) // 2
+        for ell in range(1, L + 1):
+            max_ = init_params["W" + str(ell)].T.max(axis=0).reshape(-1, 1)
+            min_ = init_params["W" + str(ell)].T.min(axis=0).reshape(-1, 1)
+            prev_conn = int(connections[ell - 1] / len(max_))
+            upper = np.tile(max_, (1, prev_conn)).flatten()
+            lower = np.tile(min_, (1, prev_conn)).flatten()
+            bounds += [(l, u) for l, u in zip(lower, upper)]
+        return bounds
+
+    # def _random_forward_pass(self, parameters, afunc, bounds, connections, biases, rng):
+    #     """random pass through network for initialization"""
+    #     # x = rng.uniform(bounds[0], bounds[1], size=np.sum(connections) + np.sum(biases))
+    #     x = np.array([rng.uniform(l, u) for (l, u) in bounds])
+    #     params = vector_to_matrices(parameters, connections, biases, x)
+    #     return linear_forward(self.Y, params, afunc)
+
+    def _random_forward_pass(self, parameters, afunc, bounds, connections, biases, rng):
         """random pass through network for initialization"""
-        x = rng.uniform(bounds[0], bounds[1], size=np.sum(connections))
-        params = vector_to_matrices(parameters, connections, x)
-        return linear_forward(self.Y, params, afunc)
+        x = np.array([rng.uniform(l, u) for (l, u) in bounds])
+        params = vector_to_matrices(parameters, connections, biases, x)
+        return linear_forward(self.Y[..., 0], params, afunc)
 
     def _reshape_vector(self, vector, dhids, dh_lens, fill_value):
         """reshape mixture vector into dh matrix"""
